2c5e3a0054970f84eb49047693995b6ef34e3820
helenaj <helenaj14@ru.is>
2016-01-30 00:57:27 +0000

    Create shape.js

---
+++ shape.js
@@ -0,0 +1,39 @@
+var Shape = Base.extend({
+
+	constructor:function(name,mouse) {
+		this.name = name;
+		this.pos = null;
+		this.size = new Point(0,0);
+		this.color = color;
+		this.lineWidth = lineWidth;
+		this.selected = false;
+		
+	},
+
+
+	draw:function(canvas) {		
+		if ( this.selected === true ) {
+			// show selection
+		}
+	},
+
+	startDrawing:function(point) {
+
+	},
+
+	drawing:function(point) {
+
+	},
+
+	stopDrawing:function(point) {
+
+	},
+
+	added: function(canvas) {
+
+	},
+
+	contains: function(point) {
+
+	},
+});
